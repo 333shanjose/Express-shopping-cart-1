67fc7fa65842c2f88a6c2de7f5940f62c5c1bbd7
333shanjose <109017031+333shanjose@users.noreply.github.com>
2022-08-10 12:38:11 +0530

    Delete collection.js

--- collection.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    PRODUCT_HELPERS:"pretty",
-    ADMIN_COLLECTION:"bds",
-    USER_COLLECTIONS:"usersf",
-    CART_COLLECTIONS :"cartsnsss",
-    ORDER_COLLECTIONS:"ornrilee"
-
-}
